b8813a8a19413fecb7a202eaae318ccdd5909a9b
tank <187276728@qq.com>
2025-12-01 00:24:20 +0800

    feat: metadata service实现

--- apps/metadata/src/infrastructure/persistence/mod.rs
+++ apps/metadata/src/infrastructure/persistence/mod.rs
@@ -1,3 +1,6 @@
 pub mod entity;
+<<<<<<< HEAD
 pub mod future;
+=======
+>>>>>>> d64570d (feat: metadata service实现)
 pub mod query;
--- apps/metadata/src/infrastructure/repository/mod.rs
+++ apps/metadata/src/infrastructure/repository/mod.rs
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 pub mod metadata_repository_impl;
+=======
+pub mod metadata_repository_impl;
+pub mod future;
+>>>>>>> d64570d (feat: metadata service实现)
